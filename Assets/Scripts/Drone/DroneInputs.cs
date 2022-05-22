85f43a2bcd5b40defa7a5088022bb067fabcae44
Ömer Faruk <bayomerfg@gmail.com>
2022-05-22 16:54:41 +0300

    Add Platform to scene

--- Assets/Scripts/Drone/DroneInputs.cs
+++ Assets/Scripts/Drone/DroneInputs.cs
@@ -11,23 +11,22 @@ namespace OmerFG
     {
         #region Variables
 
+        [SerializeField] GameObject lightObj;
+
         private Vector2 cyclic;
         private float pedals;
         private float throttle;
+        private float shoot;
+        private float light;
 
         public Vector2 Cyclic { get => cyclic; }
         public float Pedals { get => pedals; }
         public float Throttle { get => throttle; }
+        public float Shoot { get => throttle; }
+        public float Light { get => throttle; }
 
         #endregion
 
-        #region Main Methods
-
-        void Update()
-        {
-
-        }
-        #endregion
 
         #region Input Methods
         void OnCyclic (InputValue value)
@@ -41,6 +40,17 @@ namespace OmerFG
         private void OnThrottle(InputValue value)
         {
             throttle = value.Get<float>();
+        }
+        private void OnShoot(InputValue value)
+        {
+            shoot = value.Get<float>();
+           
+        } 
+        private void OnLight(InputValue value)
+        {
+            light = value.Get<float>();
+            if (light > 0)
+                lightObj.SetActive(!lightObj.active);
         }
         #endregion
 
